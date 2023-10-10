b7f7d3b36bccdbfdb1976ffcf98ac8352f27790b
Shishir Halder <s.halder0108@gmail.com>
2023-10-10 06:42:42 +0600

    feat: implement firebase via authProvider in registration page

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,8 +1,90 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import { FcGoogle } from 'react-icons/fc'
+import { Link } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { AuthContext } from '../../Providers/AuthProvider';
+import swal from 'sweetalert';
+
 
 const Register = () => {
+  const [registerError, setRegisterError] = useState('');     // an empty string evaluated to be false
+  const [registerSuccess, setRegisterSuccess] = useState('');
+  const [showPass, setShowPass] = useState(false);
+
+  const notify = (message) => toast.error(message);
+  const { createUser } = useContext(AuthContext);
+
+  const handleRegister = e => {
+    e.preventDefault();
+
+    setRegisterError('');
+    setRegisterSuccess('');
+
+    
+    const email = e.target.email.value;     // from name attribute of input:email
+    const password = e.target.password.value;
+    console.log(email, password);
+    
+    if(password.length < 6) {
+      setRegisterError('Password must be of 6 characters atleast');
+      notify('Password must be of 6 characters atleast');
+      return;
+    }
+    
+    createUser(email, password)
+      .then((userCredential) => {
+        // Signed up 
+        const user = userCredential.user;
+        swal("Thank You!", "For creating an account!", "success");
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+        notify(errorMessage)
+      });
+    
+  }
+
   return (
-    <div>Register</div>
+    <div>
+        <div className='w-fit mx-auto mt-10 rounded-3xl overflow-hidden shadow-2xl'>
+            <div className='text-center px-8 pt-6'>
+                <h2 className="text-purple-700 text-3xl font-bold">Register Here</h2>
+                <p className='text-xl font-light mt-2'>
+                    Create an account to explore
+                    <br />
+                    our services
+                </p>
+            </div>
+            <form onSubmit={handleRegister} action="">
+              <div className='px-8 my-8 flex flex-col'>
+                  <input type="email" name="email" placeholder='Enter your email here' className='outline-purple-600 p-2 border-2 border-black rounded-md' />
+                  <div className='mt-8 flex items-center'>
+                      <input type={showPass ? 'text' : 'password'} name="password" placeholder='Enter your password here' className='outline-purple-600 p-2 border-2 border-black rounded-md w-full' />
+                      <span onClick={() => {setShowPass(!showPass)}} className='cursor-pointer -ml-7'>
+                        {showPass ? <FaEyeSlash /> : <FaEye />}
+                      </span>
+                  </div>
+              </div>
+              <div className='w-full my-1  px-8'>
+                  <input type="submit" value="Register" className='bg-purple-600 text-white text-lg font-medium w-full py-1 rounded-md cursor-pointer' />
+              </div>
+            </form>
+            <div className='mx-8 flex justify-center items-center gap-2 p-1 text-sm mb-8'>
+                <p className='font-light'>Already have an account?</p>
+                <Link to={'/login'} className='text-purple-800 font-medium'>Login</Link>
+            </div>
+            <div className='px-8 mb-6'>
+                <div className='flex justify-center items-center'><hr className='grow' /><span className='px-4'>OR</span><hr className='grow' /></div>
+                <button className='flex justify-start items-center gap-2 text-lg border-2 border-gray-300 rounded-md w-full py-1 pl-2 mt-4'><FcGoogle className='text-2xl' /><span>Sign-up with google</span></button>
+            </div>
+        </div>
+        <ToastContainer position='bottom-right' />
+    </div>
   )
 }
 
---
+++ src/Providers/AuthProvider.jsx
@@ -0,0 +1,25 @@
+import React, { createContext, useState } from 'react'
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import auth from "../firebase/firebase.init"
+
+export const AuthContext = createContext(null);
+
+const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null);
+
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const authInfo = { user, createUser }
+
+    return (
+    <div>
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    </div>
+    )
+}
+
+export default AuthProvider
